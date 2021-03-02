bd84178d353e427cfb913d3fc118233029063271
Teodóra Vékony <45353438+vekteo@users.noreply.github.com>
2021-03-02 11:07:08 +0100

    create languages.js
    Create new js file for instructions in different languages

---
+++ languages.js
@@ -0,0 +1,103 @@
+const en = {
+    welcomePage: {
+        welcome: "Welcome!",
+        clickNext: "Click on <strong>Next</strong> to continue!"
+    },
+    instruction: {
+        numbers: "In this task, <strong>digits</strong> will be presented on the screen, once at a time.",
+        oneSecond: "Each digit will be presented for one second. After that, a new number will be presented, and so on.",
+        yourTask: "Your task is <strong>to try to memorize all the numbers</strong>.",
+        submit: "After a few numbers, we will ask you to type in the numbers in their order of appearance. You can submit your results by pressing the Continue button or by hitting Enter.</p><p>Pay close attention to all of the numbers, because the number of digits within a run will be longer with time.",
+        image: "<img src='static/images/instruction_en.gif' />",
+        clickNext: "If the instructions are clear, click on <strong>Next</strong>!"
+    },
+    practice: {
+        practice: "First, you can practice the task a little bit.",
+        startPractice: "Press any key on the keyboard to start the practice!",
+        afterPractice: "Good! The task start now. From now on, you won't receive feedback about your answer.",
+        startTask: "Press any button to start the task!",
+        end: "End of the practice."
+    },
+    end:  {
+        end: "End of the task.",
+        thankYou: "Thank you for participating!",
+    },
+    button: {
+        next: "Next",
+        previous: "Previous",
+        submit: "Next"
+    },
+    fullscreen: {
+        fullscreenText: "The experiment will switch to full screen mode when you press the button below.",
+        fullscreenButton: "Continue"
+    },
+    feedback: {
+        correct: "Correct!",
+        wrong: "Wrong!",
+        correctIs: "The correct answer is: ",
+        nextPractice: "Press any key for the next practice trial!",
+        yourAnswer: "Your answer was ",
+        toContinue: "Press any key to continue!",
+        answerIs: "The answer is: ",
+        longestStream2: "The longest stream you remembered correctly contained 2 numbers.",
+        longestStream: "The longest run you remembered correctly contained ",
+        numbers: " numbers."
+    },
+    task: {
+        start: "The task start now. From now on, you won't receive any feedback.",
+        press: "Press any key to start the task!",
+        whatNumbers: "What numbers have you seen?",
+        enter: "Enter them <strong> in order </strong>the text box below!"
+    }
+}
+
+const hu = {
+    welcomePage: {
+        welcome: "Üdvözlünk!",
+        clickNext: "Kattints a <strong>Tovább</strong> gombra a folytatáshoz!"
+    },
+    instruction: {
+        numbers: "Ebben a feladatban <strong>számok</strong> fognak megjelenni a képernyőn egymás után.",
+        oneSecond: "Mindegyik számot egy másodpercig fogod látni a képernyőn. Ezt követően egy újabb szám következik, és így tovább.",
+        yourTask: "A feladatod az lesz, hogy <strong>próbáld meg megjegyezni az összes számot</strong>.",
+        submit: "Néhány szám bemutatása után arra fogunk kérni, hogy gépeld be a megjelenő mezőbe a látott számokat a megjelenés sorrendjében.</p><p>Válaszodat a Tovább gombra való kattintással, vagy az Enter lenyomásával hagyhatod jóvá.</p><p>Jól figyelj az összes számra, mert az idő előrehaladtával a bemutatott számok mennyisége növekedni fog.",
+        image: "<img src='static/images/instruction_hu.gif' />",
+        clickNext: "Ha világosak az instrukciók, kattints a <strong>Tovább</strong> gombra!"
+    },
+    practice: {
+        practice: "Most gyakorolhatod egy kicsit a feladatot.",
+        startPractice: "Nyomd meg bármelyik billentyűt a gyakorlás kezdéséhez!",
+        end: "Vége a gyakorlásnak."
+    },
+    end:  {
+        end: "Vége a feladatnak.",
+        thankYou: "Köszönjük a részvételt!",
+    },
+    button: {
+        next: "Tovább",
+        previous: "Vissza",
+        submit: "Tovább"
+    },
+     fullscreen: {
+         fullscreenText: "A feladat teljes képernyőre ugrik, amint megnyomod a Tovább gombot.",
+         fullscreenButton: "Tovább"
+    },
+    feedback: {
+        correct: "Helyes!",
+        wrong: "Helytelen!",
+        correctIs: "A helyes válasz: ",
+        nextPractice: "Nyomd meg bármelyik billentyűt a következő gyakorló feladathoz!",
+        yourAnswer: "A válaszod: ",
+        toContinue: "Nyomd meg bármelyik billentyűt a folytatáshoz!",
+        answerIs: "A válasz: ",
+        longestStream2: "A leghosszabb sorozat, amelyre helyesen válaszoltál 2 számot tartalmazott",
+        longestStream: "A leghosszabb sorozat, amelyre helyesen válaszoltál ",
+        numbers: " számot tartalmazott."
+    },
+    task: {
+        start: "A feladat most kezdődik élesben. Mostantól nem fog visszajelzést kapni arról, hogy helyesen válaszoltál-e.",
+        press: "Nyomd meg bármelyik billentyűt a kezdéshez!",
+        whatNumbers: "Milyen számokat láttál?",
+        enter: "Gépeld be a számokat az alábbi szövegdobozba <strong> a megjelenés sorrendjében</strong>!"
+    }
+}
