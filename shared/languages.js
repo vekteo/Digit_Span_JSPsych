590de5d0ad6d63311eab82a84b4e62f9a1ddf785
Teodóra Vékony <45353438+vekteo@users.noreply.github.com>
2021-05-03 10:59:42 +0200

    create shared folder for files shared between online and offline version

---
+++ shared/languages.js
@@ -0,0 +1,312 @@
+const en = {
+    welcomePage: {
+        welcome: "Welcome!",
+        clickNext: "Click on <strong>Next</strong> to continue!"
+    },
+    instruction: {
+        numbers: "In this task, <strong>digits</strong> will be presented on the screen, once at a time.",
+        oneSecond: "Each digit will be presented for one second. After that, a new number will be presented, and so on.",
+        yourTask: "Your task is <strong>to try to memorize all the digits</strong>.",
+        submit: "After a few digits, we will ask you to type in the digits <strong>in their order of appearance</strong>. You can submit your results by pressing the Continue button or by hitting Enter. After you typed the numbers you can forget them.</p><p>Pay close attention to all of the numbers, because the number of digits within a run will be longer with time.",
+        image: "<img src='../static/images/instruction_en.gif' />",
+        clickNext: "If the instructions are clear, click on <strong>Next</strong>!"
+    },
+    practice: {
+        practice: "First, you can practice the task a little bit.",
+        startPractice: "Press any key on the keyboard to start the practice!",
+        afterPractice: "Good! The task start now. From now on, you won't receive feedback about your answer.",
+        startTask: "Press any button to start the task!",
+        end: "End of the practice."
+    },
+    end:  {
+        end: "End of the task.",
+        thankYou: "Thank you for participating!",
+    },
+    button: {
+        next: "Next",
+        previous: "Previous",
+        submit: "Next"
+    },
+    fullscreen: {
+        fullscreenText: "The experiment will switch to full screen mode when you press the button below.",
+        fullscreenButton: "Continue"
+    },
+    feedback: {
+        correct: "Correct!",
+        wrong: "Wrong!",
+        correctIs: "The correct answer is: ",
+        nextPractice: "Press any key for the next practice trial!",
+        yourAnswer: "Your answer was ",
+        toContinue: "Press any key to continue!",
+        answerIs: "The answer is: ",
+        longestStream2: "The longest stream you remembered correctly contained 2 numbers.",
+        longestStream: "The longest run you remembered correctly contained ",
+        numbers: " numbers."
+    },
+    task: {
+        start: "The task start now. From now on, you won't receive any feedback.",
+        press: "Press any key to start the task!",
+        whatNumbers: "What numbers have you seen?",
+        enter: "Enter them <strong> in order </strong>the text box below!"
+    },
+    parameters: {
+        subject: "Subject Number:",
+        session: "Session Number:"
+    },
+    startWarning: {
+        startSubject: "You are about to start the task with<strong> Subject Number ",
+        startSession: "</strong> and <strong>Session Number ",
+        startButton: "Start the task!",
+        goBackButton: "Change the parameters"
+    },
+}
+
+const hu = {
+    welcomePage: {
+        welcome: "Üdvözlünk!",
+        clickNext: "Kattints a <strong>Tovább</strong> gombra a folytatáshoz!"
+    },
+    instruction: {
+        numbers: "Ebben a feladatban <strong>számok</strong> fognak megjelenni a képernyőn egymás után.",
+        oneSecond: "Mindegyik számot egy másodpercig fogod látni a képernyőn. Ezt követően egy újabb szám következik, és így tovább.",
+        yourTask: "A feladatod az lesz, hogy <strong>próbáld meg megjegyezni az összes számot</strong>.",
+        submit: "Néhány szám bemutatása után arra fogunk kérni, hogy gépeld be a megjelenő mezőbe a látott számokat <strong>a megjelenés sorrendjében</strong>.</p><p>Válaszodat a Tovább gombra való kattintással, vagy az Enter lenyomásával hagyhatod jóvá.</p><p>Jól figyelj az összes számra, mert az idő előrehaladtával a bemutatott számok mennyisége növekedni fog.",
+        image: "<img src='../static/images/instruction_hu.gif' />",
+        clickNext: "Ha világosak az instrukciók, kattints a <strong>Tovább</strong> gombra!"
+    },
+    practice: {
+        practice: "Most gyakorolhatod egy kicsit a feladatot.",
+        startPractice: "Nyomd meg bármelyik billentyűt a gyakorlás kezdéséhez!",
+        end: "Vége a gyakorlásnak."
+    },
+    end:  {
+        end: "Vége a feladatnak.",
+        thankYou: "Köszönjük a részvételt!",
+    },
+    button: {
+        next: "Tovább",
+        previous: "Vissza",
+        submit: "Tovább"
+    },
+     fullscreen: {
+         fullscreenText: "A feladat teljes képernyőre ugrik, amint a Tovább gombra kattintasz.",
+         fullscreenButton: "Tovább"
+    },
+    feedback: {
+        correct: "Helyes!",
+        wrong: "Helytelen!",
+        correctIs: "A helyes válasz: ",
+        nextPractice: "Nyomd meg bármelyik billentyűt a következő gyakorló feladathoz!",
+        yourAnswer: "A válaszod: ",
+        toContinue: "Nyomd meg bármelyik billentyűt a folytatáshoz!",
+        answerIs: "A válasz: ",
+        longestStream2: "A leghosszabb sorozat, amelyre helyesen válaszoltál 2 számot tartalmazott",
+        longestStream: "A leghosszabb sorozat, amelyre helyesen válaszoltál ",
+        numbers: " számot tartalmazott."
+    },
+    task: {
+        start: "A feladat most kezdődik élesben. Mostantól nem fog visszajelzést kapni arról, hogy helyesen válaszoltál-e.",
+        press: "Nyomd meg bármelyik billentyűt a kezdéshez!",
+        whatNumbers: "Milyen számokat láttál?",
+        enter: "Gépeld be a számokat az alábbi szövegdobozba <strong> a megjelenés sorrendjében</strong>!"
+    },
+    parameters: {
+        subject: "Vizsgálati személy száma: ",
+        session: "Ülés száma: "
+    },
+    startWarning: {
+        startSubject: "Feladat kezdése <strong> ezzel a vizsgálati személy számmal: ",
+        startSession: "</strong> és <strong>ezzel az ülésszámmal: ",
+        startButton: "Feladat kezdése",
+        goBackButton: "Paraméterek változtatása"
+    },
+}
+
+const es = {
+    welcomePage: {
+        welcome: "Bienvenid@!",
+        clickNext: "Haz clic en <strong>Siguiente</strong> para continuar!"
+    },
+    instruction: {
+        numbers: "En esta tarea, <strong>números</strong> te serán presentados en la pantalla, uno a uno.",
+        oneSecond: "Cada dígito aparecerá por un segundo.",
+        yourTask: "Tu objetivo es intentar de <strong>memorizar todos los números </strong>.",
+        submit: "Luego de haberte presentado algunos números, te pediremos de escribirlos con el teclado <strong>en el orden en el que los viste aparecer</strong>. Puedes enviar tus respuestas presionando el botón Continuar o presionando Enter.</p><p>Pon mucha atención a todos los números puesto que la cantidad de dígitos irá aumentando.  Después de haber indicado todos los números, puedes olvidarlos.",
+        image: "<img src='../static/images/instruction_en.gif' />",
+        clickNext: "Si comprendiste las instrucciones, haz clic en <strong>Siguiente</strong>!"
+    },
+    practice: {
+        practice: "Empezaremos con un pequeño entrenamiento.",
+        startPractice: "Haz clic en cualquier tecla para empezar el entrenamiento!",
+        afterPractice: "Bien hecho! La tarea empezará a continuación. A partir de ahora, no recibirás ningún comentario en relación a tus respuestas.",
+        startTask: "Haz clic en cualquier tecla para comenzar!",
+        end: "Fin del entrenamiento."
+    },
+    end:  {
+        end: "Fin de la tarea.",
+        thankYou: "Gracias por haber participado!",
+    },
+    button: {
+        next: "Siguiente",
+        previous: "Atrás",
+        submit: "Siguiente"
+    },
+    fullscreen: {
+        fullscreenText: "El experimento pasará a pantalla completa al cliquear en Continuar.",
+        fullscreenButton: "Continuar"
+    },
+    feedback: {
+        correct: "Correcto!",
+        wrong: "Incorrecto!",
+        correctIs: "La respuesta correcta es: ",
+        nextPractice: "Haz clic en cualquier tecla para el siguiente ensayo!",
+        yourAnswer: "Tu respuesta fue ",
+        toContinue: "Haz clic en cualquier tecla para continuar!",
+        answerIs: "La respuesta es: ",
+        longestStream2: "La secuencia más larga de dígitos que has retenido contiene 2 números.",
+        longestStream: "La secuencia más larga de dígitos que has retenido contiene ",
+        numbers: " números."
+    },
+    task: {
+        start: "La tarea comenzará ahora. A partir de ahora no recibirás ningún comentario.",
+        press: "Haz clic en cualquier tecla para continuar!",
+        whatNumbers: "¿Qué números viste?",
+        enter: "Indícalos  <strong> en orden </strong>en la siguiente casilla!"
+    },
+    parameters: {
+        subject: "Número de asunto: ",
+        session: "Número de sesión: "
+    },
+    startWarning: {
+        startSubject: "Está a punto de iniciar la tarea con <strong>Número de Asunto ",
+        startSession: "</strong> y <strong>Número de Sesión ",
+        startButton: "¡Inicia la tarea!",
+        goBackButton: "Cambiar los parámetros"
+    },
+}
+
+const fr = {
+    welcomePage: {
+        welcome: "Bienvenue!",
+        clickNext: "Cliquez sur <strong>Suivant</strong> pour continuer!"
+    },
+    instruction: {
+        numbers: "Dans cette tâche, <strong>des chiffres</strong> vont apparaître sur l'écran, un à un.",
+        oneSecond: "Chaque chiffre va apparaître durant une seconde. Après cela, un nouveau chiffre apparaîtra, et ainsi de suite.",
+        yourTask: "Votre tâche est <strong>d'essayer de mémoriser tous les chiffres</strong>.",
+        submit: "Après quelques chiffres, il vous sera demandé de taper les chiffres <strong>dans leur ordre d'apparition</strong>. Vous pouvez soumettre votre résultat en appuyant sur le bouton Continuer ou en appuyant sur la touche Entrée.</p><p>Faites bien attention à tous les chiffres, car le nombre de chiffres va augmenter de plus en plus. Après avoir tapé les chiffres, vous pouvez les oublier.",
+        image: "<img src='../static/images/instruction_en.gif' />",
+        clickNext: "Si les instructions sont claires, cliquez sur <strong>Suivant</strong>!"
+    },
+    practice: {
+        practice: "Tout d'abord, nous allons nous entraîner à la tâche.",
+        startPractice: "Appuyez sur n'importe quelle touche de votre clavier pour commencer l'entraînement!",
+        afterPractice: "Bien! La tâche va maintenant commencer. A partir de maintenant, vous ne recevrez plus de retour sur vos réponses.",
+        startTask: "Appuyez sur n'importe quelle touche de votre clavier pour commencer la tâche!",
+        end: "Fin de l'entraînement."
+    },
+    end:  {
+        end: "Fin de la tâche.",
+        thankYou: "Merci de votre participation!",
+    },
+    button: {
+        next: "Suivant",
+        previous: "Précédent",
+        submit: "Suivant"
+    },
+    fullscreen: {
+        fullscreenText: "L'expérience passera en mode plein écran lorsque vous appuierez sur le bouton ci-dessous.",
+        fullscreenButton: "Continuer"
+    },
+    feedback: {
+        correct: "Correct!",
+        wrong: "Faux!",
+        correctIs: "La réponse correcte est: ",
+        nextPractice: "Appuyez sur n'importe quelle touche pour démarrer le prochain bloc d'entraînement!",
+        yourAnswer: "Votre réponse était ",
+        toContinue: "Appuyer sur n'importe quelle touche pour continuer!",
+        answerIs: "La réponse est: ",
+        longestStream2: "Le plus long jet dont vous vous êtes souvenu correctement contenait 2 chiffres.",
+        longestStream: "Le plus long jet dont vous vous êtes souvenu correctement contenait ",
+        numbers: " chiffres."
+    },
+    task: {
+        start: "La tâche va maintenant commencer. A partir de maintenant, vous ne recevrez plus de retour sur vos réponses.",
+        press: "Touchez n'importe quelle touche pour commencer la tâche!",
+        whatNumbers: "Quels chiffres avez-vous vu?",
+        enter: "Entrez-les <strong> dans l'ordre </strong>dans le bloc de texte ci dessous!"
+    },
+    parameters: {
+        subject: "Numéro du sujet : ",
+        session: "Numéro de session : "
+    },
+    startWarning: {
+        startSubject: "Vous êtes sur le point de commencer la tâche avec <strong>Numéro de sujet ",
+        startSession: "</strong> et <strong>Numéro de session ",
+        startButton: "Commencez la tâche !",
+        goBackButton: "Modifier les paramètres"
+    },
+}
+
+const pt = {
+    welcomePage: {
+        welcome: "Bem-vindo!",
+        clickNext: "Clique em <strong>Próximo</strong> para continuar!"
+    },
+    instruction: {
+        numbers: "Nessa tarefa, <strong>dígitos</strong> serão apresentados na tela, um por vez.",
+        oneSecond: "Cada dígito será apresentado por um segundo. Depois disso, um novo número, e assim por diante.",
+        yourTask: "A sua tarefa consiste em <strong>tentar memorisar todos os números</strong>.",
+        submit: "Depois de alguns números, nos vamos te pedir para digitá-los <strong>na ordem em que eles apareceram</strong>. Você pode enviar seus resultados apertando o botão continuar ou a tecla enter.</p><p>Preste bastante atenção em todos os números, pois o número de digitos por série aumentará a cada vez. Depois que digitar os números, você pode esquecê-los.",
+        image: "<img src='../static/images/instruction_en.gif' />",
+        clickNext: "Se as instruções estão claras, clique em <strong>Próximo</strong>!"
+    },
+    practice: {
+        practice: "Primeiro, você pode treinar a tarefa um pouquinho.",
+        startPractice: "Aperte qualquer tecla para iniciar o treino!",
+        afterPractice: "Muito bem! A tarefa começa agora. De agora em diante, você não receberá nenhum feedback de suas respostas.",
+        startTask: "Aperte qualquer tecla para iniciar a tarefa!",
+        end: "Fim do treino."
+    },
+    end:  {
+        end: "Fim da tarefa.",
+        thankYou: "Muito obrigado por participar!",
+    },
+    button: {
+        next: "Próximo",
+        previous: "Anterior",
+        submit: "Próximo"
+    },
+    fullscreen: {
+        fullscreenText: "O experimento irá passar para o modo tela cheia quando você clicar no botão abaixo.",
+        fullscreenButton: "Continue"
+    },
+    feedback: {
+        correct: "Correto!",
+        wrong: "Errado!",
+        correctIs: "A resposta correta é: ",
+        nextPractice: "Aperte qualquer tecla para a proxima sessão de treino!",
+        yourAnswer: "Sua resposta foi ",
+        toContinue: "Aperte qualquer tecla para continuar!",
+        answerIs: "A resposta é: ",
+        longestStream2: "A sequência mais longa que você se lembrou corretamente continha 2 números.",
+        longestStream: "A sequência mais longa que você se lembrou corretamente continha ",
+        numbers: " números."
+    },
+    task: {
+        start: "A tarefa começa agora. De agora em diante, você não receberá nenhum feedback.",
+        press: "Aperte qualquer tecla para iniciar a tarefa!",
+        whatNumbers: "Que números você viu?",
+        enter: "Insira <strong> em ordem </strong>na caixa de texto abaixo!"
+    },
+    parameters: {
+        subject: "Assunto número: ",
+        session: "Número da sessão: "
+    },
+    startWarning: {
+        startSubject: "Você está prestes a iniciar a tarefa com<strong> Número do assuntot ",
+        startSession: "</strong> e <strong>Sessão número ",
+        startButton: "Comece a tarefa!",
+        goBackButton: "Alterar os parâmetros"
+    },
+}
